4b1b2dca5eb08c2fdc9dd9b06e32fc7bea9fedd0
Alexey S <lexx@42point.com>
2017-12-30 08:48:27 +0300

    initial commit

---
+++ assets/js/functions.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    $(function() {
+        $('.menu__icon').on('click', function() {
+            $('.navbar').slideToggle(300, function() {
+
+                if ($(this).css('display') === 'none') {
+                    $(this).removeAttr('style');
+                }
+            });
+        });
+    });
+
+});
